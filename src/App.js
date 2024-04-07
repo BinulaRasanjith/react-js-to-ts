b1c80c6576188bd41bf59fef0c88932f054781b2
Binula Rasanjith <binula28@gmail.com>
2024-04-07 14:18:01 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import router from "./router";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
 
 function App() {
   return (
